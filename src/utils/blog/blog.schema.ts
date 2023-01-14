f9e5a745f3fa8536d5313610fc7a5620912de4a9
Jacopo Marrone <jacopo.marrone27@gmail.com>
2023-01-14 01:38:33 +0100

    feat: add  blogpost validation during build

---
+++ src/utils/blog/blog.schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { Logger } from "./utils.logger";
+
+const blogPostSchema = z.object({
+  /** Thrse fields are required! Don't edit unless you know what you are doing */
+  slug: z.string(),
+  title: z.string(),
+  contentAsHTMLString: z.string(),
+}).extend({
+  /* Add here your front-matter markdown custom fields */
+});
+
+export type BlogPost = z.infer<typeof blogPostSchema>;
+
+// Validation
+export const validateBlogPostOrThrow = (blogPost: BlogPost) => {
+  const result = blogPostSchema.safeParse(blogPost);
+  if (result.success) return;
+  const logger = new Logger("BlogPost Not Valid");
+  const errorMessage = logger.error(
+    "BlogPost slug: " + blogPost.slug,
+    "Some attributes of the blogpost are not valid, usually means that blogpost frontmatter has something wrong.",
+    result.error,
+  );
+  throw new Error(errorMessage);
+};
--- src/utils/blog/index.ts
+++ src/utils/blog/index.ts
@@ -2,7 +2,9 @@ import { readdirSync, readFileSync } from "fs";
 import path from "path";
 import matter from 'gray-matter';
 import { parseMarkdownIntoHTMLString } from "./utils.markdown";
+import { BlogPost, validateBlogPostOrThrow } from "./blog.schema";
 import { capitalize } from "./utils.string";
+export { type BlogPost };
 
 const getBlogDirPath = () => path.resolve(process.cwd(), "./src/blog-contents");
 export const getAllBlogPostSlugs = () => readdirSync(getBlogDirPath()).map(filename => filename.replace('.md', ''));
@@ -20,14 +22,20 @@ export const getBlogPostBySlug = (slug: string): BlogPost => {
   const file = getBlogPostFileBySlug(slug);
   const metadata = matter(file.toString());
   const markdownAsString = metadata.content;
-  const markdownAsHTMLString = parseMarkdownIntoHTMLString(markdownAsString);
-  return {
+  const contentAsHTMLString = parseMarkdownIntoHTMLString(markdownAsString);
+  const blogPost: BlogPost = {
+    // required fields
     slug,
     title: metadata.data.title ?? capitalize(slug.replaceAll('-', ' ')),
-    content: {
-      markdownAsHTMLString,
-    },
+    contentAsHTMLString,
+    // custom fields
+    excerpt: metadata.data.excerpt,
+    date: metadata.data.date,
+    author: metadata.data.author,
+    coverImage: metadata.data.coverImage,
   };
+  validateBlogPostOrThrow(blogPost);
+  return blogPost;
 };
 export const getAllBlogPosts = (): BlogPost[] => {
   const slugs = getAllBlogPostSlugs();
